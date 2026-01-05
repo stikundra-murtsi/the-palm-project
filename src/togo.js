f10d90e87c9d6b4096b2a864ad908c5c9a485178
stikundra-murtsi <m@stikkot.ru>
2026-01-05 00:29:53 +0000

    first commit

---
+++ src/togo.js
@@ -0,0 +1,14 @@
+export function togo(a) {
+	location.href = `${a}`;
+}
+
+let shop = document.getElementById("to_shop");
+let general = document.getElementById("site_general");
+
+if (shop != null) {
+	shop.onclick = () => togo("../shop.html");
+}
+
+if (general != null) {
+	general.onclick = () => togo("../index.html");
+}
